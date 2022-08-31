9639a0246dd482152e2cf453b131378bc6e8be62
Gangula2 <gangulaanwesh@gmail.com>
2022-08-31 12:58:03 +0530

    add folder in destination + allow all img types

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import sharp from 'sharp';
 
 const create = (sourceFile, destinationPath, s) => {
     const fileName = path.parse(sourceFile).name;
+    const fileExt = path.parse(sourceFile).ext;
     new Promise((rs, rj) => {
         sharp(sourceFile)
             .rotate()
             .resize(s, s)
-            .toFile(`${destinationPath}/${fileName}x${s}.png`)
+            .toFile(`${destinationPath}/${fileName}x${s}${fileExt}`)
             .then((d) => rs())
             .catch((e) => {
                 rj(e);
@@ -18,11 +19,15 @@ const create = (sourceFile, destinationPath, s) => {
 }
 
 export async function resize(sourceFile, destinationPath, sizes) {
+    destinationPath = path.join(destinationPath, "web-ext-icons")
     await new Promise((rs, rj) =>
         rimraf(destinationPath, { disableGlob: true }, () => rs())
     );
     if (!fs.existsSync(destinationPath)) {
         fs.mkdirSync(destinationPath);
     }
-    await Promise.all(sizes.map((s) => create(sourceFile, destinationPath, s)));
+    await Promise.all(sizes.map((s) => create(sourceFile, destinationPath, s)))
+        .then(
+            console.log("Resized files to this location: " + destinationPath)
+        );
 };
